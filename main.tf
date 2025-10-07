fe960bb6c8b5ceb0b0ac22bd9e1870f9b9587a8f
ale.alencarr <alexandre-879@hotmail.com>
2025-10-07 14:33:17 -0300

    feat: deploy yml

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "random_string" "suffix" {
 
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acrtchungry${random_string.suffix.result}"
+  name                = "acrtchungry-hnaia0"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   sku                 = "Basic"
