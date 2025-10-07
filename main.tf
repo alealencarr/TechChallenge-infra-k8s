6f717fd3336225057a59bebeaa256d336433aa8f
ale.alencarr <alexandre-879@hotmail.com>
2025-10-07 14:35:21 -0300

    update name acr

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "random_string" "suffix" {
 
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acrtchungry-hnaia0"
+  name                = "acrtchungryprod"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   sku                 = "Basic"
